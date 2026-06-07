0cea1418e067e73958c5405ff2cce18f8ef045d2
shaharia-islam-bappy-2532758 <shaharia.bappy.253@northsouth.edu>
2026-06-07 23:35:16 +0600

    update.

---
+++ Variable.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+
+    //variable = A reusable container for a value.
+    //           Behaves as if it were the value it contains.
+    //           There are different datat type
+
+    int age = 25;
+    float gpa = 2.28; // you can add up to six or seven digit after decimal.
+    double pi =3.1416784566;
+
+
+    printf("Your age is %d.\n", age);
+    printf("Your gpa is %f. \n", gpa);
+    printf("The value of pi is %lf. \n", pi);
+
+
+    return 0;
+}
